1ab982ccf9c8a5fd158428395325ed001e8dabca
gitoak <74053774+gitoak@users.noreply.github.com>
2025-10-26 17:30:14 +0100

    remove boilerplate

---
+++ lib/app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'loomday',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('loomday')),
+      body: const Center(child: Text('Hello, loomday!')),
+    );
+  }
+}
